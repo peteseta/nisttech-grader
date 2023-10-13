96dede590eb392b21cb6f4e0e24adcc1e56fc199
peteseta <petesetabandhu@gmail.com>
2023-10-13 15:30:38 +0700

    fix logout route

--- src/routes/leaderboard/+page.svelte
+++ src/routes/leaderboard/+page.svelte
@@ -1,7 +1,7 @@
 <slot />
 
 <div class="flex flex-col m-6 space-y-6 md:space-y-0 md:space-x-6 md:flex-row">
-// TODO: For all users, get their total score and rank them. Can have a nice podium for top 3
+// TODO: For all users, get their total score and rank them. Can have a nice podium for top 3.
 // TODO: Can also add a section for the problem breakdown. like for each problem, how many people solved it, how many people attempted it, etc.
 // don't forget, since this is sveltekit we do database stuff in /src/routes/leaderboard/+page.server.js. see docs.
 </div>
