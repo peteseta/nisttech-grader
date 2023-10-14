3c478829326d94c336d0c293bae3df762f1f4454
peteseta <petesetabandhu@gmail.com>
2023-10-14 22:32:07 +0700

    refactor + change to table view

--- src/routes/leaderboard/+page.svelte
+++ src/routes/leaderboard/+page.svelte
@@ -1,52 +1,29 @@
-<slot />
-
-<div class="flex flex-col m-6 space-y-6 md:space-y-0 md:space-x-6 md:flex-row justify-center">
-  <ul role="list" class="divide-y divide-gray-100">
-    {#each {length: data_length} as _, i}
-        <li class="flex justify-between gap-x-6 py-5">
-            <div class="flex min-w-0 gap-x-4">
-                {#if i == 0}
-                    <p class="text-sm font-semibold leading-6 text-orange-300 mr-5">{data.submissions[i].user_name}</p>
-                    <div class="min-w-0 flex-auto">
-                        <p class="text-sm font-semibold leading-6 text-orange-300 mr-5">{data.submissions[i].total_points}</p>
-                    </div>
-                    <div class="min-w-0 flex-aut">
-                        <p class="text-sm font-semibold leading-6 text-orange-300">{data.submissions[i].school}</p>
-                    </div>
-                {:else if i == 1}
-                    <p class="text-sm font-semibold leading-6 text-slate-500 mr-5">{data.submissions[i].user_name}</p>
-                    <div class="min-w-0 flex-auto">
-                        <p class="text-sm font-semibold leading-6 text-slate-500 mr-5">{data.submissions[i].total_points}</p>
-                    </div>
-                    <div class="min-w-0 flex-aut">
-                        <p class="text-sm font-semibold leading-6 text-slate-500">{data.submissions[i].school}</p>
-                    </div>
-                {:else if i == 2}
-                    <p class="text-sm font-semibold leading-6 text-orange-700 mr-5">{data.submissions[i].user_name}</p>
-                    <div class="min-w-0 flex-auto">
-                        <p class="text-sm font-semibold leading-6 text-orange-700 mr-5">{data.submissions[i].total_points}</p>
-                    </div>
-                    <div class="min-w-0 flex-aut">
-                        <p class="text-sm font-semibold leading-6 text-orange-700">{data.submissions[i].school}</p>
-                    </div>
-                {:else}
-                    {data.submissions.user_name}
-                    <div class="min-w-0 flex-auto">
-                        <p class="text-sm font-semibold leading-6 text-gray-900 mr-5">{data.submissions[i].total_points}</p>
-                    </div>
-                    <div class="min-w-0 flex-auto">
-                        <p class="text-sm font-semibold leading-6 text-gray-900 mr-5">{data.submissions[i].school}</p>
-                    </div>
-                {/if}
-            </div>
-        </li>
-    {/each}
-  </ul>
-</div>
-
-// TODO: Can also add a section for the problem breakdown. like for each problem, how many people solved it, how many people attempted it, etc.
-
 <script>
     export let data;
     export let data_length = Object.keys(data.submissions).length;
 </script>
+
+<div class="flex justify-center m-6 md:space-y-0 md:space-x-6 md:flex-row">
+    <table class="w-screen table-auto md:max-w-3xl">
+        <thead>
+            <tr>
+                <th class="px-4 py-2 font-bold">Place</th>
+                <th class="px-4 py-2 font-bold">Name</th>
+                <th class="px-4 py-2">School</th>
+                <th class="px-4 py-2">Score</th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each {length: data_length} as _, i}
+                <tr class="{i == 0 ? 'text-orange-300' : i == 1 ? 'text-slate-500' : i == 2 ? 'text-orange-700' : 'text-gray-900'}">
+                    <td class="px-4 py-2 font-semibold border">{i + 1}</td>
+                    <td class="px-4 py-2 font-semibold border">{data.submissions[i].user_name}</td>
+                    <td class="px-4 py-2 text-sm font-semibold border">{data.submissions[i].school}</td>
+                    <td class="px-4 py-2 text-sm font-semibold border">{data.submissions[i].total_points} points</td>
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+</div>
+
+<!-- TODO: Can also add a section for the problem breakdown. like for each problem, how many people solved it, how many people attempted it, etc. -->
