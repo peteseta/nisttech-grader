ed632d8e5b944e6b51b64fff6efbc85ae295dfdc
peteseta <petesetabandhu@gmail.com>
2023-10-21 21:23:52 +0700

    min problem number to 0

--- src/lib/form.svelte
+++ src/lib/form.svelte
@@ -8,7 +8,7 @@
 <div class="flex flex-col p-6 space-y-4 bg-white rounded-lg border {width} items-left border-slate-300">
     <h2 class="m-1 text-xl font-bold text-slate-700">submit a new attempt</h2>
     <form class="space-y-4" on:submit|preventDefault={handleSubmit}>
-        <input type="number" bind:value={problemNumber} placeholder="problem number" class="p-3 w-full rounded-md border" required/>
+        <input type="number" bind:value={problemNumber} placeholder="problem number" class="p-3 w-full rounded-md border" required min="0"/>
         <input type="file" bind:files={file} accept=".py,.c,.cpp,.java,.js" placeholder="Upload solution file (.py, .c, .cpp, .java, .js)" class="p-4 w-full rounded-md border" required/>
         <button type="submit" class="px-4 py-3 font-bold text-white bg-orange-400 rounded-md hover:bg-orange-500 hover:text-orange-100" required>
             submit solution
